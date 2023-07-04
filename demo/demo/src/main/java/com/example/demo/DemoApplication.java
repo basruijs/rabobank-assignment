2ebeeb53e8c864f2c4957737118db36747f9dd24
basruijs <62066420+basruijs@users.noreply.github.com>
2023-07-04 09:25:08 +0200

    Update DemoApplication.java

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,13 +10,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-//		Card card = new Card(124L, "30/06", true, 1000);
-//		Account user = new Account("Bas", "Ruijs");
-//		Card card2 = new Card(123L, "30/06", true, 1000);
-//		card.withdrawMoney(1000);
-//		card.depositMoney(1);
-//		card.depositMoney(1000000000);
-//		System.out.println(card.getCardNr());
 
 	}
 
